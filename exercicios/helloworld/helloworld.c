45f5c9495d0acc20e30b34935ffe1fb945fd109a
Glyson <kauaglyson@gmail.com>
2022-03-16 20:44:35 -0300

    Adicionando novos exercícios
    Teste

---
+++ exercicios/helloworld/helloworld.c
@@ -0,0 +1,8 @@
+// Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+#include <stdio.h>
+int main(void)
+{
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ exercicios/pedir e imprimir numero/pedir_e_imprimir_numero.c
@@ -0,0 +1,12 @@
+// Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+#include <stdio.h>
+
+int main(void)
+{
+    int numero = 0;
+    printf("Por favor, digite um número:\n");
+    scanf("%d", &numero);
+
+    printf("O número informado foi: %d", numero);
+    return 0;
+}
