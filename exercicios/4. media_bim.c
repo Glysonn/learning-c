cec746d5278150c0125aff517b0f0677f393d71d
Glyson <kauaglyson@gmail.com>
2022-03-19 01:32:27 -0300

    Exercício 4
    Pedir 4 notas ao usuário e imprimir a média aritmética dessas notas.

--- exercicios/4. media_bim.c
+++ exercicios/4. media_bim.c
@@ -3,16 +3,24 @@
 
 int main(void)
 {
-    float n1, n2, n3, n4, media;
+    // declarando variáveis.
+    float n1, n2, n3, n4;
+
+    // pedindo valores de cada variável ao usuário.
     printf("Digite sua nota 1:\n");
     scanf("%f", &n1);
+
     printf("Digite sua nota 2:\n");
     scanf("%f", &n2);
+
     printf("Digite sua nota 3:\n");
     scanf("%f", &n3);
+
     printf("Digite sua nota 4:\n");
     scanf("%f", &n4);
 
+    // imprimindo o resultado
+    // o cálculo da média foi feito no método printf(), por isso não foi necessário criar uma variável para isso.
     printf("Sua média final é: %f", (n1 + n2 + n3 + n4) / 4);
     return 0;
 }
