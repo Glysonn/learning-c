130178f6bf3048b3bda92284dc9b5cec43733491
Glyson <kauaglyson@gmail.com>
2022-03-19 01:35:37 -0300

    Exercício 5
    Fazendo a conversão de metros para centímetros.

--- exercicios/5. metros_para_cm.c
+++ exercicios/5. metros_para_cm.c
@@ -3,11 +3,15 @@
 
 int main(void)
 {
+    // declarando a variável e pedindo um valor para ela.
     float num;
     printf("Digite a quantidade (em metros): ");
     scanf("%f", &num);
 
+    // criando uma variável com a fórmula de conversão e realizando o cálculo.
     int cm = num * 100;
+
+    // imprimindo resultado da conversão.
     printf("Resultado: %d %s", cm, "cm");
     return 0;
 }
