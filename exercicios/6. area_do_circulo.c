7f467800bc49eacd1512abe5f50791b6598d164b
Glyson <83619472+Glysonn@users.noreply.github.com>
2022-03-19 22:19:45 -0300

    Exercício 6
    Calculando a área do círculo a partir do raio que o usuário escolher.

--- exercicios/6. area_do_circulo.c
+++ exercicios/6. area_do_circulo.c
@@ -3,9 +3,12 @@
 
 int main(void)
 {
+    // declarando a variável do raio e atribuindo um valor input nela.
     float r;
     printf("Qual o raio do círculo?\n");
     scanf("%f", &r);
+
+    // calculando a área do círculo e imprimindo.
     float area = 3.1415926 * (r * r);
     printf("%s %.4f\b %s", "A área é aproximadamente: ", area, "m²");
     return 0;
